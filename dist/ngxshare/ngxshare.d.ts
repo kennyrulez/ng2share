48b921af9ef5c64f5c4155998e140f08afdaf58b
Valerio Fornito <fornito.valerio@gmail.com>
2019-01-14 10:50:07 +0100

    version 2.1.1 updated packages to fix webpack vulnerability

--- dist/ngxshare/ngxshare.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { ShareButtonComponent as ɵa } from './lib/share-button/share-button.component';
--- dist/ngxshare/public_api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/ngxshare.component';
-export * from './lib/ngxshare.module';
